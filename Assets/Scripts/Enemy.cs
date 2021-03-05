530ff38609ac52329eefdfbd7292f822aac8a322
wOUShPT <gustavogomesmaldonado@hotmail.com>
2021-03-05 11:33:54 +0000

    yes2

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -19,15 +19,17 @@ public class Enemy : MonoBehaviour
 
         RaycastHit hit;
         Collider[] objs;
-        objs = Physics.OverlapSphere(transform.position, 10, playerMask);
+        objs = Physics.OverlapSphere(transform.position, 6, playerMask);
         foreach (var obj in objs)
         {
             {
-                if (Vector3.Distance(obj.transform.position, transform.position) < 10)
+                if (Vector3.Distance(obj.transform.position, transform.position) < 6)
                 {
                     agent.SetDestination(obj.transform.position);
                 }
             }
         }
+
+        objs = null;
     }
 }
